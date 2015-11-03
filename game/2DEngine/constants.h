78d42588d20bde497c6fedc3a56fb70883c26861
Ronald <ronaldl@mtu.edu>
2015-11-03 12:48:51 -0500

    Things

--- game/2DEngine/constants.h
+++ game/2DEngine/constants.h
@@ -23,7 +23,6 @@
 //                  Constants
 //-----------------------------------------------
 // graphic images
-// const char BACKGROUND_IMAGE[]  = ""; 
 const char CHARACTER_IMAGE[]   = "pictures\\WallSocketLady.png";   // spaceCHARACTER
 
 // window
@@ -51,8 +50,6 @@ const float MIN_FRAME_TIME = 1.0f/FRAME_RATE;   // minimum desired time for 1 fr
 const float MAX_FRAME_TIME = 1.0f/MIN_FRAME_RATE; // maximum time used in calculations
 
 // key mappings
-// In this game simple constants are used for key mappings. If variables were used
-// it would be possible to save and restore key mappings from a data file.
 const UCHAR ESC_KEY      = VK_ESCAPE;       // escape key
 const UCHAR ALT_KEY      = VK_MENU;         // Alt key
 const UCHAR ENTER_KEY    = VK_RETURN;       // Enter key
